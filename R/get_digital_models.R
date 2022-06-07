539931ed926097f4d166221a655b3ba31f0831b5
J. Antonio Guzmán Q <antguz06@gmail.com>
2022-06-06 23:48:10 -0500

    modifications to get digital models

--- R/get_digital_models.R
+++ R/get_digital_models.R
@@ -29,11 +29,14 @@ library(sf)
 #' @param input_file Path and name of the .las file to process.
 #' @param output_name Path and name (without extension) of files to export.
 #' @param resolution Resolution of the digital models.
+#' @param DTM Use the a predefine DTM. If NULL it creates a new one. 
 #' @param clip_path A path and name of a .kml of boundaries to crop.
-#' @param threads  An interger of the number of threads to use.
+#' @param threads  An integer of the number of threads to use.
 
-input_file <- "data/2022-04-10_FAB.las"
-output_name <- "data/2022-04-10_FAB2"
+#input_file <- "data/2022-04-10_FAB.las"
+input_file <- "/media/antonio/antonio_ssd/point_clouds/2022-04-10_FAB2_clean.las"
+#output_name <- "data/2022-04-10_FAB2"
+output_name <- "/media/antonio/antonio_ssd/point_clouds/2022-04-10_FAB2"
 resolution <- 0.1
 clip_path <- "data/FAB2_blocks_buffer.gpkg"
 threads <- 26
@@ -41,73 +44,47 @@ threads <- 26
 #' -----------------------------------------------------------------------------
 #' Function
 
-get_digital_models <- function(input_file, output_name, resolution = 0.10, threads = 26) {
+get_digital_models <- function(input_file, 
+                               output_name, 
+                               resolution = 0.10, 
+                               DTM = NULL, 
+                               clip_path, 
+                               threads = 26) {
   
   #Set number of threads to use
   set_lidr_threads(threads)
   
   #Read point cloud
-  point_cloud <- readLAS(input_file)
+  pc <- readLAS(input_file)
   
   #Read clipping polygon
   clip <- st_read(dsn = clip_path)
   clip_terra <- vect(clip_path)
   
   #Get projection
-  pc_CRS <- crs(point_cloud)
+  pc_CRS <- crs(pc)
   vector <- crs(clip)
   
   if(all.equal(pc_CRS, vector) == FALSE) {
     stop("Projections of point cloud and cliping vector does not match")
   }
   
-  #Create a buffer 4 times the resolution on the clipping polygon
-  boundary <- st_union(st_buffer(clip, (resolution*4)))
-  
-  #######################
-  # Point cloud processing
-  ######################
-  
-  #Clip point cloud        ---------------------------------------------
-  extend <- st_bbox(boundary)
-  
-  #Rectangle for speed
-  pc <- clip_rectangle(point_cloud, xleft = extend[1], 
-                                    ybottom = extend[2], 
-                                    xright = extend[3], 
-                                    ytop = extend[4])
-  #ROI for details
-  pc <- clip_roi(pc, boundary)
-  
-  #Release memory
-  rm(list = c("point_cloud"))
-  gc()
-  
-  #Classify ground points  ---------------------------------------------
-  pc <- classify_ground(pc, 
-                        algorithm = csf(sloop_smooth = FALSE,
-                                        class_threshold = 0.2,
-                                        cloth_resolution = 0.2,
-                                        rigidness = 3L,
-                                        iterations = 500L,
-                                        time_step = 0.65))
-  
-  #Export
-  pc_name <- paste0(output_name, "_clip.las")
-  writeLAS(pc, pc_name, index = FALSE)
-  
-  #Digital Terrain Model  ---------------------------------------------
-  dtm <- rasterize_terrain(las = pc, 
-                           res = resolution*5,
-                           algorithm = tin())
-  
-  dtm <- terra::crop(dtm, clip_terra, mask = TRUE)
-  dtm_name <- paste0(output_name, "_DTM.tif")
-  writeRaster(dtm, dtm_name, overwrite=TRUE)
-  
-  #Release memory
-  rm(list = c("dtm", "dtm_name"))
-  gc()
+  if(is.null(DTM)) {
+    
+    #Digital Terrain Model  ---------------------------------------------
+    dtm <- rasterize_terrain(las = pc, 
+                             res = resolution,
+                             algorithm = tin())
+    
+    dtm <- terra::crop(dtm, clip_terra, mask = TRUE)
+    dtm_name <- paste0(output_name, "_DTM.tif")
+    writeRaster(dtm, dtm_name, overwrite=TRUE)
+    
+    #Release memory
+    rm(list = c("dtm_name"))
+    gc()
+    
+  }
   
   #Digital Surface Model  ---------------------------------------------
   dsm <- rasterize_canopy(las = pc, 
@@ -123,8 +100,7 @@ get_digital_models <- function(input_file, output_name, resolution = 0.10, threa
   gc()
   
   #Normalize height ---------------------------------------------------
-  pc_normalized <- normalize_height(pc, 
-                                    algorithm = tin())
+  pc_normalized <- normalize_height(pc, dtm = dtm)
   
   normalized_name <- paste0(output_name, "_normalized.las")
   writeLAS(pc_normalized, normalized_name, index = FALSE)
