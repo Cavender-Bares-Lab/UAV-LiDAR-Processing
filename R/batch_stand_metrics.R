c0811154509c1c1b1d89b87929a20aa8218e1ef7
Jose Antonio Guzman Quesada <guzman@umn.edu>
2022-06-09 15:24:25 -0500

    stand metrics batch

--- R/batch_stand_metrics.R
+++ R/batch_stand_metrics.R
@@ -15,33 +15,54 @@ library(doParallel)
 
 #-------------------------------------------------------------------------------
 #' Arguments
-#' @param point_cloud
-#' @param limits_gpkg
-#' @param output_name 
-#' @param threads
+#' @param path_pc Path to the point cloud
+#' @param path_gpkg Path to the gpkl
+#' @param output_name Path and name of the outputs
+#' @param threads Number of threads
 
-point_cloud <- readLAS("F:/point_clouds/2022-05-18_FAB2.las")
-limits_gpkg <- st_read(dsn = "data/FAB2_plots.gpkg")
-threads <- 4
+path_pc <- "Z:/9-UAV/LiDAR/2022-04-10_FAB1-2/L3/FAB2/2022-04-10_FAB2_normalized.las"
+path_gpkg <- "data/FAB2_plots.gpkg"
+point_cloud <- "Z:/9-UAV/LiDAR/2022-04-10_FAB1-2/L3/FAB2/2022-04-10_FAB2m"
+threads <- 16
 
 #' -----------------------------------------------------------------------------
 #' Function
 
-batch_stand_metrics <- function(point_cloud, limits_gpkg, output_name, threads) {
+batch_stand_metrics <- function(path_pc, path_gpkg, output_name, threads) {
+  
+  #Read point cloud
+  pc <- readLAS(path_pc)
+  
+  #Read gpkp
+  limits_gpkg <- st_read(dsn = path_gpkg)
   
   #Divide point cloud
-  pcs <- clip_roi(point_cloud, limits_gpkg)
+  pcs <- clip_roi(pc, limits_gpkg)
   
-  #Set up cluster
-  cl <- makeCluster(threads, type = "FORK")
-  registerDoParallel(cl)
+  #Release memory
+  rm(list = c("pc"))
+  gc()
   
   #N of stands
   stands <- length(pcs)
   
+  #Combine function
+  frame_combine <- function(x, b) {
+    
+    if(class(x) == "data.table" & class(b) == "data.table") {
+      frame <- rbind(x, b)
+    }
+    return(frame)
+  }
+  
+  #Set up cluster
+  cl <- makeCluster(threads)
+  #cl <- makeCluster(threads, type = "FORK")
+  registerDoParallel(cl)
+  
   # Loop over scenes to estimate delta VI
-  complete <- foreach(i = 1:stands,
-                      .combine=rbind,
+  complete <- foreach(i = 1:10,
+                      .combine= frame_combine,
                       .packages = c("lidR", "data.table", "moments", "sf", "sfheaders"),
                       .export = c("stand_metrics", "shannon"),
                       .inorder = F) %dopar% {
@@ -58,6 +79,9 @@ batch_stand_metrics <- function(point_cloud, limits_gpkg, output_name, threads) 
                          
                          #Add id
                          metrics$id <- limits_gpkg$id[i]
+                         
+                         #Reciduals
+                         gc()
                          
                          #Export
                          return(metrics)
